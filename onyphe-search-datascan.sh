a726938f32cd7761566e307d317c81a0d0fb52ba
ONYPHE <40629718+onyphe@users.noreply.github.com>
2020-04-01 15:00:08 +0200

    Update: use APIv2

--- onyphe-search-datascan.sh
+++ onyphe-search-datascan.sh
@@ -9,12 +9,12 @@ if [ -z "$SEARCH" ]; then
 	exit 1
 fi
 
-MAX_PAGE=`curl -s "https://www.onyphe.io/api/search/datascan/$SEARCH?k=$ONYPHE" | jq .max_page`
+MAX_PAGE=`curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/category:datascan%20$SEARCH" | jq .max_page`
 
 echo "*** Last page: $MAX_PAGE"
 
 for i in $(seq 1 $MAX_PAGE); do
 	echo "*** Fetching page: $i"
-	curl -s "https://www.onyphe.io/api/search/datascan/$SEARCH?k=$ONYPHE&page=$i" | jq '.results[]' -cM >> /tmp/datascan.json
+	curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/category:datascan%20$SEARCH?page=$i" | jq '.results[]' -cM >> /tmp/datascan.json
 	sleep 3
 done
