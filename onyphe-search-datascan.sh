deb92af67dd695645964ff342f7fe3b9a394df21
ONYPHE <40629718+onyphe@users.noreply.github.com>
2020-10-11 15:38:39 +0200

    Make script usable with complete OQL search string
    Can use category:CATEGORY syntax to search within all categories on information and write in a specific file for earch category.
    Example: /tmp/datascan.json, /tmp/resolver.json, ...

--- onyphe-search-datascan.sh
+++ onyphe-search-datascan.sh
@@ -5,20 +5,27 @@ ONYPHE="REPLACE_WITH_YOUR_APIKEY"
 SEARCH=$1
 
 if [ -z "$SEARCH" ]; then
-	echo "Usage: $0 \"ONYPHE Query String\""
+	echo "Usage: $0 \"ONYPHE Query Language string\""
 	exit 1
 fi
 
-MAX_PAGE=`curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/category:datascan%20$SEARCH" | jq .max_page`
+if [[ "$SEARCH" =~ category:([a-z]+) ]]; then
+	CATEGORY="${BASH_REMATCH[1]}"
+else
+	echo "*** ERROR: please start your search category:CATEGORY"
+	exit 2
+fi
+
+MAX_PAGE=`curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/$SEARCH" | jq .max_page`
 if [ "$MAX_PAGE" = "null" ]; then
 	echo "*** No result, aborting"
-	exit 2
+	exit 3
 fi
 
 echo "*** Last page: $MAX_PAGE"
 
 for i in $(seq 1 $MAX_PAGE); do
 	echo "*** Fetching page: $i"
-	curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/category:datascan%20$SEARCH?page=$i" | jq '.results[]' -cM >> /tmp/datascan.json
+	curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/$SEARCH?page=$i" | jq '.results[]' -cM >> /tmp/$CATEGORY.json
 	sleep 1
 done
