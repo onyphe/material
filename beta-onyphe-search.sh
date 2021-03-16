e0a8db5bc22b6197e5025df362ac2f636149e52e
ONYPHE <40629718+onyphe@users.noreply.github.com>
2021-03-16 15:17:16 +0100

    Create beta-onyphe-search.sh

---
+++ beta-onyphe-search.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ONYPHE="REPLACE_WITH_YOUR_APIKEY"
+
+SEARCH=$1
+
+if [ -z "$SEARCH" ]; then
+   echo "Usage: $0 \"ONYPHE Query Language string\""
+   exit 1
+fi
+
+if [[ "$SEARCH" =~ category:([a-z]+) ]]; then
+   CATEGORY="${BASH_REMATCH[1]}"
+else
+   echo "*** ERROR: please start your search with category:CATEGORY"
+   exit 2
+fi
+
+MAX_PAGE=`curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://beta.onyphe.io/testapi/v2/search/$SEARCH" | jq .max_page`
+if [ -z "$MAX_PAGE" ]; then
+   echo "*** No result, aborting"
+   exit 3
+fi
+if [ "$MAX_PAGE" = "null" ]; then
+   echo "*** No result, aborting"
+   exit 4
+fi
+
+echo "*** Last page: $MAX_PAGE"
+
+for i in $(seq 1 $MAX_PAGE); do
+   echo "*** Fetching page: $i"
+   curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://beta.onyphe.io/testapi/v2/search/$SEARCH?page=$i" | jq '.results[]' -cM >> /tmp/$CATEGORY.json
+   sleep 1
+done
