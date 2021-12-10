b5254865ca025d9ac364daebc975880965866d7e
ONYPHE <40629718+onyphe@users.noreply.github.com>
2021-12-10 06:19:35 +0100

    Export API version of onyphe-search.sh

---
+++ onyphe-export.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ONYPHE="REPLACE_WITH_YOUR_APIKEY"
+
+SEARCH=$1
+
+if [ -z "$SEARCH" ]; then
+	echo "Usage: $0 \"ONYPHE Query Language string\""
+	exit 1
+fi
+
+if [[ "$SEARCH" =~ category:([a-z]+) ]]; then
+	CATEGORY="${BASH_REMATCH[1]}"
+else
+	echo "*** ERROR: please start your search with category:CATEGORY"
+	exit 2
+fi
+
+EXPORT=$(perl -MURI::Escape -e "print uri_escape(\"$SEARCH\")")
+
+curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/export/$SEARCH" >> /tmp/$CATEGORY.json
