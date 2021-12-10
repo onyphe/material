1917d81a49898d2dacb84f4bead38bc5dce98dce
ONYPHE <40629718+onyphe@users.noreply.github.com>
2021-12-10 06:38:25 +0100

    Discovery API version of onyphe-search.sh

---
+++ onyphe-discovery.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ONYPHE="REPLACE_WITH_YOUR_APIKEY"
+
+CATEGORY=$1
+INPUT=$2
+
+if [ -z "$CATEGORY" ]; then
+	echo "Usage: $0 \"category\" \"input file\""
+	exit 1
+fi
+
+if [ -z "$INPUT" ]; then
+	echo "Usage: $0 \"category\" \"input file\""
+	exit 1
+fi
+
+curl -s -XPOST -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/bulk/discovery/$CATEGORY/asset" --data-binary @$INPUT >> /tmp/$CATEGORY.json
