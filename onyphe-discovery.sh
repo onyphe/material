d9109b6baba6f6fd054ed82c5febacb91875403e
ONYPHE <40629718+onyphe@users.noreply.github.com>
2022-12-05 11:33:54 +0100

    Update onyphe-discovery.sh
    Use keepalive & fetch up to 10,000 results.

--- onyphe-discovery.sh
+++ onyphe-discovery.sh
@@ -15,4 +15,4 @@ if [ -z "$INPUT" ]; then
 	exit 1
 fi
 
-curl -s -XPOST -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/bulk/discovery/$CATEGORY/asset" --data-binary @$INPUT >> /tmp/$CATEGORY.json
+curl -s -XPOST -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/bulk/discovery/$CATEGORY/asset?keepalive=true&size=10000" --data-binary @$INPUT | grep -v '"@category":"none"' >> /tmp/$CATEGORY.json
