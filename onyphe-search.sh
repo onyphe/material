af0432ea35cae9ddf33cea67b1be34f5e9302c1d
ONYPHE <40629718+onyphe@users.noreply.github.com>
2022-12-07 08:10:08 +0100

    Update onyphe-search.sh
    Support for unrated API access with basic auth

--- onyphe-search.sh
+++ onyphe-search.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 ONYPHE="REPLACE_WITH_YOUR_APIKEY"
+AUTH=""
+
+CURL=curl
+APIURL=https://www.onyphe.io/api/v2
+if [ "$AUTH" ]; then
+   CURL="curl --basic -u $AUTH"
+   APIURL=https://www.onyphe.io/unrated/api/v2
+fi
 
 SEARCH=$1
 
 if [ -z "$SEARCH" ]; then
-	echo "Usage: $0 \"ONYPHE Query Language string\""
-	exit 1
+   echo "Usage: $0 \"ONYPHE Query Language string\""
+   exit 1
 fi
 
 if [[ "$SEARCH" =~ category:([a-z]+) ]]; then
-	CATEGORY="${BASH_REMATCH[1]}"
+   CATEGORY="${BASH_REMATCH[1]}"
 else
-	echo "*** ERROR: please start your search with category:CATEGORY"
-	exit 2
+   echo "*** ERROR: please start your search with category:CATEGORY"
+   exit 2
 fi
 
 SEARCH=$(perl -MURI::Escape -e "print uri_escape('$SEARCH')")
 
-MAX_PAGE=`curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/$SEARCH" | jq .max_page`
+MAX_PAGE=`$CURL -s -H 'Content-Type: application/json' "$APIURL/search/$SEARCH?k=$ONYPHE" | jq .max_page`
 if [ -z "$MAX_PAGE" ]; then
    echo "*** No result, aborting"
    exit 3
@@ -31,7 +39,6 @@ fi
 echo "*** Last page: $MAX_PAGE"
 
 for i in $(seq 1 $MAX_PAGE); do
-	echo "*** Fetching page: $i"
-	curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/search/$SEARCH?page=$i" | jq '.results[]' -cM >> /tmp/$CATEGORY.json
-	sleep 1
+   echo "*** Fetching page: $i"
+   $CURL -s -H 'Content-Type: application/json' "$APIURL/search/$SEARCH?k=$ONYPHE&page=$i" | jq '.results[]' -cM >> /tmp/$CATEGORY.json
 done
