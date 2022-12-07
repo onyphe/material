3468a6a0340ffc1ea2f5b38bbab5b289406e1e92
ONYPHE <40629718+onyphe@users.noreply.github.com>
2022-12-07 08:29:14 +0100

    Update onyphe-discovery.sh
    Support for unrated API with basic auth

--- onyphe-discovery.sh
+++ onyphe-discovery.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 ONYPHE="REPLACE_WITH_YOUR_APIKEY"
+AUTH=""
+
+CURL=curl
+APIURL=https://www.onyphe.io/api/v2
+if [ "$AUTH" ]; then
+   CURL="curl --basic -u $AUTH"
+   APIURL=https://www.onyphe.io/unrated/api/v2
+fi
 
 CATEGORY=$1
 INPUT=$2
 
 if [ -z "$CATEGORY" ]; then
-	echo "Usage: $0 \"category\" \"input file\""
-	exit 1
+   echo "Usage: $0 \"category\" \"input file\""
+   exit 1
 fi
 
 if [ -z "$INPUT" ]; then
-	echo "Usage: $0 \"category\" \"input file\""
-	exit 1
+   echo "Usage: $0 \"category\" \"input file\""
+   exit 1
 fi
 
-curl -s -XPOST -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/bulk/discovery/$CATEGORY/asset?keepalive=true&size=10000" --data-binary @$INPUT | grep -v '"@category":"none"' >> /tmp/$CATEGORY.json
+$CURL -s -XPOST -H 'Content-Type: application/json' "$APIURL/bulk/discovery/$CATEGORY/asset?keepalive=true&size=10000&k=$ONYPHE" --data-binary @$INPUT | grep -v '"@category":"none"' >> /tmp/$CATEGORY.json
