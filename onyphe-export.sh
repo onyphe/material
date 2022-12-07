6e119dcee42cb06ea2924907e6c810ae128b78f0
ONYPHE <40629718+onyphe@users.noreply.github.com>
2022-12-07 08:21:29 +0100

    Update onyphe-export.sh
    Support for unrated API with basic auth

--- onyphe-export.sh
+++ onyphe-export.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 ONYPHE="REPLACE_WITH_YOUR_APIKEY"
+AUTH=""
+
+CURL=curl
+APIURL=https://www.onyphe.io/api/v2
+if [ "$AUTH" ]; then
+   CURL="curl --basic -u $AUTH"
+   APIURL=https://www.onyphe.io/unrated/api/v2
+fi
 
 SEARCH=$1
 
 if [ -z "$SEARCH" ]; then
-	echo "Usage: $0 \"ONYPHE Query Language string\""
-	exit 1
+   echo "Usage: $0 \"ONYPHE Query Language string\""
+   exit 1
 fi
 
 if [[ "$SEARCH" =~ category:([a-z]+) ]]; then
-	CATEGORY="${BASH_REMATCH[1]}"
+   CATEGORY="${BASH_REMATCH[1]}"
 else
-	echo "*** ERROR: please start your search with category:CATEGORY"
-	exit 2
+   echo "*** ERROR: please start your search with category:CATEGORY"
+   exit 2
 fi
 
-EXPORT=$(perl -MURI::Escape -e "print uri_escape('$SEARCH')")
+SEARCH=$(perl -MURI::Escape -e "print uri_escape('$SEARCH')")
 
-curl -s -H "Authorization: apikey $ONYPHE" -H 'Content-Type: application/json' "https://www.onyphe.io/api/v2/export/$SEARCH" >> /tmp/$CATEGORY.json
+$CURL -s -H 'Content-Type: application/json' "$APIURL/export/$SEARCH?k=$ONYPHE" >> /tmp/$CATEGORY.json
